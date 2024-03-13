fdc2dd7ac05abd4546638371d9476e32f30845c9
Kvelez05 <kvelez1907@gmail.com>
2024-03-13 15:16:10 -0500

    METADATA AND ELEMENTS

---
+++ src/Footer.js
@@ -0,0 +1,34 @@
+export default function Footer() {
+    return (
+      <footer>
+        <img src="" alt=""/>
+        <div>
+            <h5>Navigation</h5>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </div>
+        <div>
+            <h5>Contact</h5>
+            <ul>
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+        </div>
+        <div>
+            <h5>Social Media Links</h5>
+            <ul>
+                <li>Instagram</li>
+                <li>LinkedIn</li>
+                <li>Pinterest</li>
+            </ul>
+        </div>
+      </footer>
+    );
+  }
---
+++ src/Nav.js
@@ -0,0 +1,27 @@
+export default function Nav() {
+    return (
+      <nav>
+        <img src="lemonlogo.jpg" alt="Little Lemon Logo" />
+        <ul>
+            <li>
+            <a href="/home">Home</a>
+            </li>
+            <li>
+            <a href="/about">About</a>
+            </li>
+            <li>
+            <a href="/menu">Menu</a>
+            </li>
+            <li>
+            <a href="/reservations">Reservations</a>
+            </li>
+            <li>
+            <a href="/reservations">Order Online</a>
+            </li>
+            <li>
+            <a href="/reservations">Login</a>
+            </li>
+        </ul>
+      </nav>
+    );
+  }
